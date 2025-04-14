78dea0d6cb203cf239fbb4dde57c828028c2d359
calebfrye-tamu <calebfrye@tamu.edu>
2025-04-14 15:14:24 -0500

    refactored top bar code for better expandability

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { KeyValue } from '@angular/common';
 
 
 @Component({
@@ -10,13 +10,36 @@ import { Component } from '@angular/core';
 })
 export class TopBarComponent {
 
-  pageLinks = {
+  pages = {
+    RoutePlanning: {
+      pageLink: "/route-planning",
+      pageTitle: "Route Planning"
+    },
+    LiveStatus: {
+      pageLink: "/live-status",
+      pageTitle: "Live Status"
+    },
+    Zones: {
+      pageLink: "/zones",
+      pageTitle: "Zones"
+    }
+  };
+
+  pageTitles = {
     RoutePlanning: "Route Planning",
     LiveStatus: "Live Status",
     Zones: "Zones",
-  };
-  private selectedLink = this.pageLinks.RoutePlanning;
+  }
+  public selectedLink = this.pages.RoutePlanning.pageTitle;
   public selectLink(linkText: string) {
     this.selectedLink = linkText;
+  }
+
+  /**used so the keyvalue pipe doesn't sort the pages alphabetically */
+  public doNotSort = (
+    a: KeyValue<string, { pageLink: string; pageTitle: string }>,
+    b: KeyValue<string, { pageLink: string; pageTitle: string }>
+  ): number => {
+    return 0;
   }
 }
